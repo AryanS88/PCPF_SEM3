d142b48e0bc47ab752e603d496df327cef846d3c
AryanS88 <102129863+AryanS88@users.noreply.github.com>
2022-09-20 21:06:58 +0530

    Create multi level inheritance.cpp

---
+++ multi level inheritance.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+//Base class
+class shape
+{
+  int lside;
+  int nside;
+};
+class Parallelogram : public shape{
+public:
+
+ int nside=4;
+ int base;
+
+
+ void stats()
+ {
+ cout<<"the shape has "<<nside<<" sides \n";
+ }
+
+};
+
+class rectangle:public Parallelogram{
+public :
+int base=10;
+int height=20;
+int area=base*height;
+};
+
+int main(){
+rectangle rect1;
+rect1.stats();
+cout<<"the area of rectangle is "<<rect1.area;
+}
+/*
+output:
+the shape has 4 sides 
+the area of rectangle is 200
+*/
